95d781553912a8c065764423970d097df09f99b1
Gracie <m.graciemcguire@gmail.com>
2020-02-27 14:24:35 -0500

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://woke-or-joke.herokuapp.com/'
+    origins 'http://woke-or-joke.herokuapp.com'
 
     resource '*',
       headers: :any,
